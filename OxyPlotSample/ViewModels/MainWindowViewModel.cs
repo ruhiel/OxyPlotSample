528e9d55b2b51d7ee6c81928a6beaabce4e5a4d9
ruhiel_murrue <ruhiel1983@gmail.com>
2017-05-10 22:42:49 +0900

    初回コミット

---
+++ OxyPlotSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxyPlotSample.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            DataList = new List<DataPoint>
+            {
+                {new DataPoint(0, 0)},
+                {new DataPoint(2, 4)},
+                {new DataPoint(5, 8)},
+                {new DataPoint(8, 3)},
+                {new DataPoint(12, 5)},
+            };
+        }
+
+        public List<DataPoint> DataList { get; }
+    }
+}
